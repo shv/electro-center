a46a24107c34a571ae492ff1bafc4ae4fe9942e2
shv <alexey@sharypov.ru>
2016-06-15 11:46:41 +0300

    change database to sqlite

--- ihome/settings.py
+++ ihome/settings.py
@@ -77,17 +77,17 @@ WSGI_APPLICATION = 'ihome.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
-    # }
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'ihome',
-        'USER': 'shv',
-        'PASSWORD': '',
-        'HOST': ''
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3')
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'ihome',
+    #     'USER': 'shv',
+    #     'PASSWORD': '',
+    #     'HOST': ''
+    # }
 }
 
 
